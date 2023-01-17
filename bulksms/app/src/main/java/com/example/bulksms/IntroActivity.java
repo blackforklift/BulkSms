93fe73282ea8a2cd031868d6d1626de70d745a09
dilan saskin <dilansskn@gmail.com>
2023-01-17 06:13:48 +0300

    -register

--- bulksms/app/src/main/java/com/example/bulksms/IntroActivity.java
+++ bulksms/app/src/main/java/com/example/bulksms/IntroActivity.java
@@ -1,15 +1,25 @@
 package com.example.bulksms;
 
 import androidx.appcompat.app.AppCompatActivity;
+import android.widget.Toast;
 
+import android.content.Intent;
 import android.os.Bundle;
 
-public class IntroActivity extends AppCompatActivity {
+import com.google.firebase.auth.FirebaseAuth;
 
+public class IntroActivity extends AppCompatActivity {
+    FirebaseAuth lAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_intro2);
-        
+        findViewById(R.id.intro_kayit).setOnClickListener(view -> {startActivity(new Intent(IntroActivity.this,RegisterActivity.class));});
+        lAuth = FirebaseAuth.getInstance();
+
+        if(lAuth.getCurrentUser() != null){
+            //startActivity(new Intent(IntroActivity.this, MainActivity.class));
+            Toast.makeText(IntroActivity.this, "Yönlendiriliyorsunuz", Toast.LENGTH_SHORT).show();
+        }
     }
 }
--- bulksms/app/src/main/java/com/example/bulksms/RegisterActivity.java
+++ bulksms/app/src/main/java/com/example/bulksms/RegisterActivity.java
@@ -2,13 +2,43 @@ package com.example.bulksms;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class RegisterActivity extends AppCompatActivity {
-
+    EditText editTextEmail,editTextPassword;
+    Button register_kayit,register_giris;
+    FirebaseAuth lauth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_register2);
+        setContentView(R.layout.activity_register);
+        editTextEmail = findViewById(R.id.editTextEmail);
+        editTextPassword=findViewById(R.id.editTextPassword);
+        register_kayit = findViewById(R.id.register_kayit);
+        register_giris = findViewById(R.id.register_giris);
+
+        lauth=FirebaseAuth.getInstance();
+        //register_giris.setOnClickListener(view -> {startActivity(new Intent(RegisterActivity.this,LoginActivity.class));});
+        register_kayit.setOnClickListener(view -> {
+           String email = editTextEmail.getText().toString();
+           String password = editTextPassword.getText().toString();
+           lauth.createUserWithEmailAndPassword(email,password).addOnCompleteListener(task -> {
+               if(task.isSuccessful())
+               {
+                   Toast.makeText(RegisterActivity.this,"KAYIT BAŞARILI",Toast.LENGTH_SHORT).show();
+               }
+               else{
+                   Toast.makeText(RegisterActivity.this,"KAYIT BAŞARISIZ",Toast.LENGTH_SHORT).show();
+               }
+           });
+
+
+        });
     }
 }
